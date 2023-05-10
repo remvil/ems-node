b18e029da1cbe5bd04bd3506c9ac15fccf850394
remvil <rvildacci@gmail.com>
2023-05-11 00:36:01 +0200

    employee rough crud

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const employees = [
+	{
+		employeeId: "1",
+		employeeName: "Aditya Gupta",
+		employeePost: "Manager",
+		employeeSalary: "43000",
+	},
+	{
+		employeeId: "2",
+		employeeName: "Vanshita Jaiswal",
+		employeePost: "Assistant Manager",
+		employeeSalary: "21000",
+	},
+];
 
 const app = express();
+
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(
@@ -10,10 +26,60 @@ app.use(
 	})
 );
 
-app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+	res.render("home", {
+		data: employees,
+	});
+});
 
-app.get("/", (req, res) => {
-	res.render("home", {variableName: "Welcome home!"});
+app.post("/", (req, res) => {
+	const inputEmployeeId = employees.length + 1;
+	const inputEmployeeName = req.body.employeeName;
+	const inputEmployeePost = req.body.employeePost;
+	const inputEmployeeSalary = req.body.employeeSalary;
+
+	employees.push({
+		employeeId: inputEmployeeId,
+		employeeName: inputEmployeeName,
+		employeePost: inputEmployeePost,
+		employeeSalary: inputEmployeeSalary,
+	});
+
+	res.render("home", {
+		data: employees,
+	});
+});
+
+app.post("/delete", (req, res) => {
+	var requestedEmployeeId = req.body.employeeId;
+	var j = 0;
+	employees.forEach((employee) => {
+		j = j + 1;
+		if (employee.employeeId === requestedEmployeeId) {
+			employees.splice(j - 1, 1);
+		}
+	});
+	res.render("home", {
+		data: employees,
+	});
+});
+
+app.post("/update", (req, res) => {
+	const requestedEmployeeId = req.body.employeeId;
+	const inputEmployeeName = req.body.employeeName;
+	const inputEmployeePost = req.body.employeePost;
+	const inputEmployeeSalary = req.body.employeeSalary;
+
+	var j = 0;
+	employees.forEach((employee) => {
+		j = j + 1;
+		if (employee.employeeId == requestedEmployeeId) {
+			(employee.employeeName = inputEmployeeName), (employee.employeePost = inputEmployeePost), (employee.employeeSalary = inputEmployeeSalary);
+		}
+	});
+	res.render("home", {
+		data: employees,
+	});
 });
 
 app.listen(3000, (req, res) => {
